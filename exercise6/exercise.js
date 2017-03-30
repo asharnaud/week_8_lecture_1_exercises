b4158b9131ae6de58cc8aa807350a44a84240670
Ashleigh Arnaud <ashleigh.arnaud@gmail.com>
2017-03-29 20:13:57 -0500

    fixed a standard issue

--- exercise6/exercise.js
+++ exercise6/exercise.js
@@ -1,6 +1,6 @@
 function currentTime () {
   var today = new Date()
-   document.getElementById('time').innerHTML = today.toLocaleTimeString()
+  document.getElementById('time').innerHTML = today.toLocaleTimeString()
 }
 
 var everySecond = 1 * 1000
